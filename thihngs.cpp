3dd2176a2482ec3e91d03807015760b0275c49ae
Charlie <gamerboy030607@gmail.com>
2023-08-12 02:00:10 +1000

    Add project files.

---
+++ thihngs.cpp
@@ -0,0 +1,76 @@
+// thihngs.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <thread>
+int main()
+{
+    int name;
+    std::cout << "Hello World!\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "welcome to the intro program for OpenStudio!\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "this program was built in c++\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "and uses the thread module and IOstream for output!\n";
+    std::cout << "whats your name developer?\n";
+    std::cin >> name;
+    std::cout << "so your name is?\n: " << name << std::endl;
+    std::cout << "nice! i like your name\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "i allmost forgot to tell you my name.\n";
+    std::cout << "it's charlie!";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "the one that started OpenStudio!\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "here is a list of projects as of friday the 8th 2023\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "\n-------------------------------\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "OpenStudioIDE\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "OpenStudioLauncher\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Pyton\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "The New OpenStudioCorp site\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "---------------------------\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "and here are our developers!\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "------------------------\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "sourena\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "exedice\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "charlie_san\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "DieDerick87\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Prosurestring\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "a750\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "syembol\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "samuel venable\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "TheBugCoder\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "erni\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "AlanVR\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Beatzoid\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "KiyoKiyotaka\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "thanks for reading our program! much love!\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "OpenStudio.\n";
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "plus come join our discord at https://discord.gg/7cFCB8qBkf \n";
+} 
+
